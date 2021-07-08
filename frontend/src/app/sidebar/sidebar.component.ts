d3f78705a50de19e9d98f76818d875e179405002
Ivan Oliveira <ivan.ribeiro@ruralsmart.com.br>
2021-07-08 14:34:16 -0300

    bkp

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  el: any;
+  el2: any;
+  loading: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  add() {
+
+  }
+
+  activeToogle() {
+    this.el = document.querySelector(".sidebar");
+    this.el2 = document.querySelector("#btn");
+
+    if (this.el.classList.contains("active")) {
+      this.el.classList.remove("active");
+    }
+    else {
+      this.el.classList.add("active");
+    }
+
+    if (this.el2.classList.contains("bx-menu")) {
+      this.el2.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+      this.el2.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  }
+
+  /*
+  <script>
+  let btn = document.querySelector("#btn");
+  let sidebar = document.querySelector(".sidebar");
+  let searchBtn = document.querySelector(".bx-search");
+  
+  btn.onclick = function () {
+      sidebar.classList.toggle("active");
+      if (btn.classList.contains("bx-menu")) {
+          btn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else {
+          btn.classList.replace("bx-menu-alt-right", "bx-menu");
+      }
+  }
+  searchBtn.onclick = function () {
+      sidebar.classList.toggle("active");
+  }
+  </script>*/
 
 }
